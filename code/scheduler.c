c318783441ec435031b2897b96dc11b747c6d11e
Ghaithq <gaoia123@gmail.com>
2023-04-09 14:48:14 +0200

    First Commit

---
+++ code/scheduler.c
@@ -0,0 +1,14 @@
+#include "headers.h"
+
+int main(int argc, char * argv[])
+{
+    //initClk();
+    printf("%c\n",*argv[0]);
+
+
+    //TODO implement the scheduler :)
+    //upon termination release the clock resources.
+    while(1){}
+    //destroyClk(true);
+}
+
